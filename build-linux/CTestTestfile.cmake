2756013a56606418963ec9118406b71a5b46e486
kareem <heisshaik@gmail.com>
2025-10-29 21:27:55 -0700

    tests

--- build-linux/CTestTestfile.cmake
+++ build-linux/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(CacheTests "/mnt/c/Users/Heisk/OneDrive/Documents/project/high-perf-cache/build-linux/cache_tests")
+set_tests_properties(CacheTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Heisk/OneDrive/Documents/project/high-perf-cache/CMakeLists.txt;48;add_test;/mnt/c/Users/Heisk/OneDrive/Documents/project/high-perf-cache/CMakeLists.txt;0;")
